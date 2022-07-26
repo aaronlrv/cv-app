d202d5bb930afd221d6ad4d59c5d7eef302641a7
aaronlrv <aaronvitharanage@gmail.com>
2022-07-26 15:52:12 +0400

    Add form for personal details

--- cv-project/src/components/Form.js
+++ cv-project/src/components/Form.js
@@ -1,7 +1,29 @@
-function Form (){
-    return (
-
-    )
+function Form() {
+  return (
+    <form>
+      <legend> Input details</legend>
+      <fieldset>
+        <div className="personal">
+          <div className="nameDiv">
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="numberDiv">
+            <label htmlFor="number">Number:</label>
+            <input type="text" name="number" id="number" />
+          </div>
+          <div className="emailDiv">
+            <label htmlFor="email">Email:</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div className="locationDiv">
+            <label htmlFor="location">Location:</label>
+            <input type="text" name="location" id="location" />
+          </div>
+        </div>
+      </fieldset>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
