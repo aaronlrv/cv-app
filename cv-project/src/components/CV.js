ca5860de82e5021c03e09d5bd7f916b600b2baf9
aaronlrv <aaronvitharanage@gmail.com>
2022-07-27 14:19:43 +0400

    Add layout to personal details

--- cv-project/src/components/CV.js
+++ cv-project/src/components/CV.js
@@ -1,7 +1,12 @@
 function CV(props) {
   return (
-    <div className="h">
-      <h3>Hi</h3>
+    <div className="cv">
+      <div className="personalDetai">
+        <h1>{props.name} </h1>
+        <h3>{props.email}</h3>
+        <h3>{props.number}</h3>
+        <h3>{props.location}</h3>
+      </div>
     </div>
   );
 }
