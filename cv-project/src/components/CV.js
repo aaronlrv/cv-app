4b211a1706b55764b552e34946d2ad70fa51c03b
aaronlrv <aaronvitharanage@gmail.com>
2022-07-27 14:27:13 +0400

    Add education layout to CV

--- cv-project/src/components/CV.js
+++ cv-project/src/components/CV.js
@@ -7,6 +7,13 @@ function CV(props) {
         <h3>{props.number}</h3>
         <h3>{props.location}</h3>
       </div>
+
+      <div className="Education">
+        <h1>{props.course}</h1>
+        <h3>{props.school}</h3>
+        <p>{props.startDate}</p>
+        <p>{props.endDate}</p>
+      </div>
     </div>
   );
 }
