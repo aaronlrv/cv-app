08116cab3ecdaffef47db15eb47080852c9359ab
aaronlrv <aaronvitharanage@gmail.com>
2022-07-27 18:45:39 +0400

    Format Work Experience section for CV Preview

--- cv-project/src/components/CV.js
+++ cv-project/src/components/CV.js
@@ -21,7 +21,7 @@ function CV(props) {
         <hr class="w-auto h-1 bg-gray-300 0 border-r-2"></hr>
       </div>
 
-      <div className="Education" class="grid grid-cols-3 grid-rows-1">
+      <div className="Education" class="grid grid-cols-3 grid-rows-1 mb-5">
         <div className="right" class="flex flex-col">
           <h1>{props.course}</h1>
           <h3>{props.school}</h3>
@@ -30,13 +30,29 @@ function CV(props) {
           <p>{props.startDate} - </p>
           <p>{props.endDate}</p>
         </div>
+        <div className="right" class="flex flex-col">
+          <h1>{props.course2}</h1>
+          <h3>{props.school2}</h3>
+        </div>
+        <div className="left" class="flex gap-3 justify-end col-start-3 pl-2">
+          <p>{props.startDate2} - </p>
+          <p>{props.endDate2}</p>
+        </div>
       </div>
 
-      <div className="workExperience">
-        <h1>{props.job}</h1>
-        <h1>{props.workPlace}</h1>
-        <h3>{props.workStart}</h3>
-        <h3>{props.workEnd}</h3>
+      <div class="mt-5">
+        <h2 class="">Work Experience</h2>
+        <hr class="w-auto h-1 bg-gray-300 0 border-r-2"></hr>
+      </div>
+      <div class="grid grid-cols-3 grid-rows-1 mb-5">
+        <div className="right" class="flex flex-col">
+          <h1>{props.job}</h1>
+          <h3>{props.workPlace}</h3>
+        </div>
+        <div className="left" class="flex gap-3 justify-end col-start-3 pl-2">
+          <p>{props.workStart} - </p>
+          <p>{props.workEnd}</p>
+        </div>
       </div>
     </div>
   );
