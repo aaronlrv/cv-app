0fd0103bb7e755440b5761f2068396e38f3a5638
aaronlrv <aaronvitharanage@gmail.com>
2022-07-29 17:54:50 +0400

    Allow button to not be rendered with cv

--- cv-project/src/components/CV.js
+++ cv-project/src/components/CV.js
@@ -3,77 +3,81 @@ import { jsPDF } from "jspdf";
 
 function CV(props) {
   return (
-    <div className="cv" id="capture">
-      <div
-        className="personalDetai"
-        class="flex flex-col bg-slate-300 h-500 h-20 justify-center align-center m-3"
-      >
-        <div class="flex flex-row justify-center align-center text-3xl underline">
-          <p>{props.name} </p>
+    <div>
+      <div className="cv" id="capture">
+        <div
+          className="personalDetai"
+          class="flex flex-col bg-slate-300 h-500 h-20 justify-center align-center m-3"
+        >
+          <div class="flex flex-row justify-center align-center text-3xl underline">
+            <p>{props.name} </p>
+          </div>
+          <div class="flex flex-row justify-center align-center gap-5">
+            <h3>{props.email}</h3>
+
+            <h3>{props.number}</h3>
+            <h3>{props.location}</h3>
+          </div>
         </div>
-        <div class="flex flex-row justify-center align-center gap-5">
-          <h3>{props.email}</h3>
 
-          <h3>{props.number}</h3>
-          <h3>{props.location}</h3>
+        <div class="mt-5">
+          <h2 class="mb-2">Summary</h2>
+          <hr class="w-auto h-1 bg-gray-300 0 border-r-2"></hr>
+        </div>
+
+        <div className="summary">
+          <p> {props.summary} </p>
+        </div>
+
+        <div class="mt-5">
+          <h2 class="mb-2">Education</h2>
+          <hr class="w-auto h-1 bg-gray-300 0 border-r-2"></hr>
+        </div>
+
+        <div className="Education" class="grid grid-cols-3 grid-rows-1 mb-5">
+          {props.education.map((x) => (
+            <>
+              <div className="right" class="flex flex-col">
+                <h3 class="bg-red-500">{x.school}</h3>
+                <h3 class="bg-red-700">{x.course}</h3>
+              </div>
+              <div
+                className="left"
+                class="flex gap-3 justify-end col-start-3 pl-2"
+              >
+                <p class="bg-green-400">{x.startDate} - </p>
+                <p class="bg-green-700">{x.endDate}</p>
+              </div>
+            </>
+          ))}
+        </div>
+
+        <div class="mt-5">
+          <h2 class="mb-2">Work Experience</h2>
+          <hr class="w-auto h-1 bg-gray-300 0 border-r-2"></hr>
+        </div>
+        <div class="grid grid-cols-3 grid-rows-1 mb-5">
+          {props.work.map((x) => (
+            <>
+              <div className="right" class="flex flex-col">
+                <h1>{x.jobRole}</h1>
+                <h3>{x.workPlace}</h3>
+                <h3>{x.summary}</h3>
+              </div>
+              <div
+                className="left"
+                class="flex gap-3 justify-end col-start-3 pl-2"
+              >
+                <p>{x.workStart} - </p>
+                <p>{x.workEnd}</p>
+              </div>
+            </>
+          ))}
         </div>
       </div>
-
-      <div class="mt-5">
-        <h2 class="">Summary</h2>
-        <hr class="w-auto h-1 bg-gray-300 0 border-r-2"></hr>
+      <div class="flex justify-center ">
+        <button onClick={() => saveCanvas()}>Save Canvas</button>
       </div>
-
-      <div className="summary">
-        <p> {props.summary} </p>
-      </div>
-
-      <div class="mt-5">
-        <h2 class="">Education</h2>
-        <hr class="w-auto h-1 bg-gray-300 0 border-r-2"></hr>
-      </div>
-
-      <div className="Education" class="grid grid-cols-3 grid-rows-1 mb-5">
-        {props.education.map((x) => (
-          <>
-            <div className="right" class="flex flex-col">
-              <h3 class="bg-red-500">{x.school}</h3>
-              <h3 class="bg-red-700">{x.course}</h3>
-            </div>
-            <div
-              className="left"
-              class="flex gap-3 justify-end col-start-3 pl-2"
-            >
-              <p class="bg-green-400">{x.startDate} - </p>
-              <p class="bg-green-700">{x.endDate}</p>
-            </div>
-          </>
-        ))}
-      </div>
-
-      <div class="mt-5">
-        <h2 class="">Work Experience</h2>
-        <hr class="w-auto h-1 bg-gray-300 0 border-r-2"></hr>
-      </div>
-      <div class="grid grid-cols-3 grid-rows-1 mb-5">
-        {props.work.map((x) => (
-          <>
-            <div className="right" class="flex flex-col">
-              <h1>{x.jobRole}</h1>
-              <h3>{x.workPlace}</h3>
-              <h3>{x.summary}</h3>
-            </div>
-            <div
-              className="left"
-              class="flex gap-3 justify-end col-start-3 pl-2"
-            >
-              <p>{x.workStart} - </p>
-              <p>{x.workEnd}</p>
-            </div>
-          </>
-        ))}
-      </div>
-      <button onClick={() => saveCanvas()}>Save Canvas</button>
     </div>
   );
 }
@@ -95,7 +99,7 @@ function saveCanvas() {
     console.log(imgHeight);
     let doc = new jsPDF("p", "mm", "a4");
     doc.addImage(img, "PNG", 2, 0, imgWidth, imgHeight, "a", "NONE");
-    doc.save();
+    doc.save("CV");
   });
 }
 
